d65523ecc1256d66ca0a5e2220bf8e87298fff0f
andys <143337582+andys54@users.noreply.github.com>
2023-09-21 16:34:33 +0200

    first commit

---
+++ js massage generator/message_generator.js
@@ -0,0 +1,22 @@
+const generateMessage = () => {
+    const parts = [
+        ["Porsche", "Audi", "BMW", "Mercedes"],
+        [200, 300, 400, 800],
+        [1970, 1980, 1990, 2000]
+    ]
+
+    const numGen = () => {
+        return Math.floor(Math.random() * 4)
+    }
+
+    let sentence = [];
+
+    for (let part in parts) {
+        sentence.push(parts[part][numGen()])
+    }
+    let final = `Your future car is gonna be a ${sentence[0]} with ${sentence[1]} HP from ${sentence[2]}.`
+    console.log(final);
+
+}
+
+generateMessage();
